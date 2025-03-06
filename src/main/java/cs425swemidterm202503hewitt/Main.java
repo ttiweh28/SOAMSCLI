46a9a1ff4ca34a74642942b28b871784c470d1ec
ttiweht <hewitttusiime82@gmail.com>
2025-03-06 11:30:47 -0600

    Added duplicate patient to test Git actions

--- src/main/java/cs425swemidterm202503hewitt/Main.java
+++ src/main/java/cs425swemidterm202503hewitt/Main.java
@@ -13,10 +13,13 @@ public class Main {
         var Patients = List.of(
                 new Patient(1,"Hewitt","Tusiime","(456) 245 345", "h@miu.edu","28-03-1998","4th St IA"),
                 new Patient(1,"Henrietta","Atwine","(234) 278 115", "hen@miu.edu","28-12-2000","5th St IA"),
-                new Patient(1,"Hezlee","Kihembo","(456) 945 675", "hez@miu.edu","19-04-1996","6th St IA")
+                new Patient(1,"Hezlee","Kihembo","(456) 945 675", "hez@miu.edu","19-04-1996","6th St IA"),
+        new Patient(1,"Hezlee","Kihembo","(456) 945 675", "hez@miu.edu","19-04-1996","6th St IA")
+
         );
 
         Patients.forEach(System.out::println);
+
 
     }
 }
